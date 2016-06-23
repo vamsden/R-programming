cb5e212c51b35478314cde7842905768c0e839ca
vanstinger <vandykfernandes@hotmail.com>
2016-06-24 00:05:48 +0400

    Initial commit

---
+++ hello_R.R
@@ -0,0 +1,8 @@
+a <- 10
+b = "test"
+assign("c", 30) # set variable
+class(a) # determines type of variable
+is.numeric(b) # check if numeric
+nchar(b) # count characters in the string
+date_set = as.Date("2016-12-05") # sets the variable as date
+class(date_set)
